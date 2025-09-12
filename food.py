4f1fedb306f080af20a41bc106e493b8a015505d
Oleg-Limba <wangjpearl@gmail.com>
2025-09-11 17:28:04 -0700

    Update food.py context added

--- food.py
+++ food.py
@@ -5,8 +5,10 @@ class Food:
     def __init__(self):
         self.x = random.randint(20,1000)
         self.y = random.randint(20, 600)
+        # points its worth
         self.points = random.randint(3,7)
         self.trait = None 
+        # selects trait
         self.image = pygame.transform.scale_by(pygame.image.load("food.png"), 0.1 + self.points * 0.01)
         if random.random() < 0.2:
             if random.random() < 0.5:
@@ -22,4 +24,5 @@ class Food:
         self.hitbox.y = self.y
         # pygame.draw.rect(screen, "red", self.hitbox)
         screen.blit(self.image, (self.x, self.y))
+
 
